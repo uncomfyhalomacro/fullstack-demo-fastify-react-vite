b92714837401cb25edc9f6103f79dd25ba51546f
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-02-27 21:35:07 +0800

    style: run biome format
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,4 +2,4 @@ import LoginForm from "../features/auth/components/LoginForm.jsx";
 
 const LoginPage = ({ onLogin }) => LoginForm({ onLogin });
 
-export default LoginPage
+export default LoginPage;
