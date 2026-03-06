b3eec23586aabfd271fe33466b99e7bf711a05ae
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2026-03-06 13:52:30 +0800

    fix by using dialect options
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- backend/database/index.js
+++ backend/database/index.js
@@ -4,7 +4,12 @@ import { PG_DB, PG_PASSWORD, PG_USER, PROD, PG_HOST } from "../env.js";
 const sequelize = new Sequelize(PG_DB, PG_USER, PG_PASSWORD, {
 	host: PG_HOST,
 	dialect: "postgres",
-	ssl: false
+	dialectOptions: {
+		ssl: {
+			require: PROD === "prod",
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 sequelize
